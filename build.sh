fe72435a6d4f072f46c7caacf1668cbf76de32f3
Masanao TAGUCHI <mtag@jp.ibm.com>
2023-09-22 17:32:47 +0900

    latestを使うよう修正

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-#! /bin/sh
-podman build -t calibre .
+#! /bin/bash
+IMAGE_NAME=calibre
+podman build -t ${IMAGE_NAME}:latest . 
+if [ $? ]; then
+	VERSION=$(podman run --rm -it ${IMAGE_NAME}:latest /usr/bin/calibre --version | sed -e "s/^.* \([0-9.]*\).*$/\1/")
+	podman tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:$VERSION
+fi
--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ CALIBRE_CONF=~/.config/calibre
 # thanks for [Huisin](https://nnt339es.hatenablog.com/entry/2022/04/30/013450)
 CALIBRE_HOME=/root
 #xhost +local:
+IMAGE_NAME=calibre:latest
 EXT_OPTS=
 if [ ! -z "$1" ]; then
 	EXT_OPTS="-v $1:/incoming/"
@@ -18,5 +19,4 @@ podman run --rm \
        -v "${CALIBRE_CONF}":${CALIBRE_HOME}/.config/calibre/ \
        --mount type=bind,src=$HOME/.Xauthority,dst=/root/.Xauthority \
        -e DISPLAY=$DISPLAY \
-       calibre \
-       /usr/bin/calibre
+       ${IMAGE_NAME} 
