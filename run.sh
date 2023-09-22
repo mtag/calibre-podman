34c5d39912ae765191a80d252cba3f2b0473fd01
Masanao TAGUCHI <mtag@jp.ibm.com>
2023-09-22 10:28:06 +0900

    取り込み用フォルダ名をincomingに変更

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 CALIBRE_LIB=/raid6/CalibreLibrary
 CALIBRE_CONF=~/.config/calibre
-IMPORT_DIR=$1
 
 # thanks for [Huisin](https://nnt339es.hatenablog.com/entry/2022/04/30/013450)
 CALIBRE_HOME=/root
 #xhost +local:
 EXT_OPTS=
-if [ ! -z "${IMPORT_DIR}" ]; then
-	EXT_OPTS="-v '${IMPORT_DIR}:/imports/"
+if [ ! -z "$1" ]; then
+	EXT_OPTS="-v $1:/incoming/"
 fi
 mkdir -p ${CALIBRE_CONF}
 podman run --rm \
