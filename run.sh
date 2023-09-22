3666c11efd83e65c8ce4f99044ddeba1b57d2015
Masanao TAGUCHI <mtag@jp.ibm.com>
2023-09-22 09:54:07 +0900

    .Xauthorityがそのままでも使えたのでコピーを廃止&import対応(テスト中)

--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
 #! /bin/sh
 CALIBRE_LIB=/raid6/CalibreLibrary
 CALIBRE_CONF=~/.config/calibre
+IMPORT_DIR=$1
 
 # thanks for [Huisin](https://nnt339es.hatenablog.com/entry/2022/04/30/013450)
 CALIBRE_HOME=/root
 #xhost +local:
+EXT_OPTS=
+if [ ! -z "${IMPORT_DIR}" ]; then
+	EXT_OPTS="-v '${IMPORT_DIR}:/imports/"
+fi
 mkdir -p ${CALIBRE_CONF}
 podman run --rm \
        -it \
        --net host \
        -v "${CALIBRE_LIB}":/CalibreLibrary/ \
+       $EXT_OPTS \
        -v "${CALIBRE_CONF}":${CALIBRE_HOME}/.config/calibre/ \
-       --mount type=bind,src=$HOME/.Xauthority,dst=/root/.Xauthority.copy \
+       --mount type=bind,src=$HOME/.Xauthority,dst=/root/.Xauthority \
        -e DISPLAY=$DISPLAY \
        calibre \
-       /bin/bash -c " \
-       cp /root/.Xauthority.copy ${CALIBRE_HOME}/.Xauthority;  # ここがポイント2
-       chown root:root ${CALIBRE_HOME}/.Xauthority; # ここがポイント3
        /usr/bin/calibre
-       "
